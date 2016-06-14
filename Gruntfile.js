1a90c887e63e7302348aa39f67a27749ab566f83
jonesa <jonesa@cybage.com>
2016-06-14 15:48:10 +0530

    added

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,20 +23,22 @@ all:
 
 },
   
- mocha: {
-    all: {
-      src: 'test/*.js',
-      options: {
-        globals: ['assert'],
-        timeout: 3000,
-        ignoreLeaks: false,
-        grep: '*-test',
-        ui: 'bdd',
-  
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          captureFile: 'results.txt', // Optionally capture the reporter output to a file 
+          quiet: false, // Optionally suppress output to standard out (defaults to false) 
+          clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false) 
+        },
+        src: ['test/**/*.js']
       }
     }
-  }
+  
 });
+
+
 
 
 grunt.loadNpmTasks('grunt-contrib-clean');
@@ -44,7 +46,8 @@ grunt.loadNpmTasks('grunt-contrib-clean');
 grunt.loadNpmTasks('grunt-contrib-uglify');
 grunt.loadNpmTasks('grunt-contrib-jshint');
 grunt.loadNpmTasks('grunt-mocha');
-grunt.registerTask('default',['clean','uglify','jshint']);
+grunt.loadNpmTasks('grunt-mocha-Test');
+grunt.registerTask('default',['clean','uglify','jshint','mochaTest']);
 };
 
 
